c0ce9a71e185febaf3922e24161c4c2d2a157477
Eber Coaquira <ebrjose@gmail.com>
2021-12-17 00:19:57 -0400

    feat: Navitation

--- src/views/medicalCenter/MedicalCenterCreate.vue
+++ src/views/medicalCenter/MedicalCenterCreate.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-  name: 'MedicalCenterCreate.vue',
+  name: 'MedicalCenterCreateVue',
 }
 </script>
 
--- src/views/medicalUnit/MedicalUnitCreate.vue
+++ src/views/medicalUnit/MedicalUnitCreate.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-  name: 'MedifalUnitCreate.vue',
+  name: 'MedifalUnitCreateVue',
 }
 </script>
 
